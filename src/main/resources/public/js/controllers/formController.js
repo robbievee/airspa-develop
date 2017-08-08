607f057699c963e9efc9265acbf18a66c2086061
VEROPALUMBO <roberto.veropalumbo@amaris.com>
2017-08-08 18:18:55 +0200

    Fix for getting departures and arrivals in the form

--- src/main/resources/public/js/controllers/formController.js
+++ src/main/resources/public/js/controllers/formController.js
@@ -1,40 +1,39 @@
-angular.module('AirApp').controller(
-		'formController',
-		[
-				'$scope',
-				'$http',
-				function($scope, $http) {
+angular.module('AirApp').controller('formController',
+  [ '$scope', '$http', function($scope, $http) {
 
-					$http.get('http://localhost:8080/busstop/list').then(
-							function(response) {
-								$scope.busStopList = response.data;
-							});
+   $http.get('http://localhost:8080/busstop/list').then(function(response) {
+    $scope.busStopList = response.data;
+   });
 
-					$scope.myDate = new Date();
-					$scope.isOpen = false;
+   $scope.myDate = new Date();
+   $scope.isOpen = false;
 
-					$scope.fromTime = {
-						value : new Date(1970, 0, 1, 14, 00, 0)
-					};
+   $scope.fromTime = {
+    value : new Date(1970, 0, 1, 14, 00, 0)
+   };
 
-					$scope.toTime = {
-							value : new Date(1970, 0, 1, 15, 00, 0)
-						};
-					
-					$scope.showDeparture = function() {
-						var departureCode = document.getElementById(
-								"departureOption").getAttribute("value");
-						var departureName = document
-								.getElementById("departureOption").innerText;
-						var arrivalCode = document.getElementById(
-								"arrivalOption").getAttribute("value");
-						var arrivalName = document
-								.getElementById("arrivalOption").innerText;
+   $scope.toTime = {
+    value : new Date(1970, 0, 1, 15, 00, 0)
+   };
 
-						console.log(departureCode + " " + departureName);
-						console.log(arrivalCode + " " + arrivalName);
-						console.log($scope.myDate);
-						console.log($scope.fromTime.value);
-						console.log($scope.toTime.value);
-					};
-				} ]);
+   function computeStationName(code) {
+    for (let stop in $scope.busStopList) {
+     if (code === $scope.busStopList[stop].codeStop) {
+      return $scope.busStopList[stop].name;
+     }
+    }
+   }
+
+   $scope.showDeparture = function() {
+    var departureCode = $scope.departure;
+    var departureName = computeStationName($scope.departure);
+    var arrivalCode = $scope.arrival;
+    var arrivalName = computeStationName($scope.arrival);
+
+    console.log(departureCode + " " + departureName);
+    console.log(arrivalCode + " " + arrivalName);
+    console.log($scope.myDate);
+    console.log($scope.fromTime.value);
+    console.log($scope.toTime.value);
+   };
+  } ]);
